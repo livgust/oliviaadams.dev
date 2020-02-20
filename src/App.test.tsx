6f01a58724177a7419d56805a014979400d678dd
Olivia Adams <oadams@athenahealth.com>
2020-02-19 22:43:05 -0500

    in progress top nav bar

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,12 @@
 import React from "react";
+import { shallow } from "enzyme";
 import { render } from "@testing-library/react";
 import App from "./App";
+import AppBar from "@material-ui/core/AppBar";
 
-test("renders learn react link", () => {
-    const { getByText } = render(<App />);
-    const linkElement = getByText(/learn react/i);
-    expect(linkElement).toBeInTheDocument();
+it("has top nav bar", () => {
+    const app = shallow(<App />);
+    const appBar = app.find(AppBar);
+    expect(appBar.length).toBe(1);
+    expect(appBar.text()).toMatch("Olivia Adams");
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,42 @@
 import React from "react";
 //import logo from "./logo.svg";
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
 import "./App.css";
+import "typeface-roboto";
 
 function App() {
+    const classes = makeStyles((theme: Theme) =>
+        createStyles({
+            root: {
+                flexGrow: 1
+            },
+            title: {
+                flexGrow: 1
+            }
+        })
+    )();
     return (
-        <div className="App">
-            <header className="App-header">
-                {/*<img src={logo} className="App-logo" alt="logo" />*/}
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
+        <div className={classes.root}>
+            <AppBar>
+                <Typography variant="h6" className={classes.title}>
+                    Olivia Adams
+                </Typography>
+                <MenuButtons />
+            </AppBar>
         </div>
+    );
+}
+
+function MenuButtons() {
+    return (
+        <>
+            <Button>Experience</Button>
+            <Button>Posts</Button>
+            <Button>Contact</Button>
+        </>
     );
 }
 
