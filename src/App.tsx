3dc6725cdf9348f0d589b7e3f09e4e1cc91d2eab
Olivia Adams <oadams@athenahealth.com>
2020-02-22 14:40:29 -0500

    styled app bar

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import React from "react";
 import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Button from "@material-ui/core/Button";
+import ButtonGroup from "@material-ui/core/ButtonGroup";
+import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import "./App.css";
 import "typeface-roboto";
 
-function App() {
+export default function App() {
     const classes = makeStyles((theme: Theme) =>
         createStyles({
             root: {
@@ -21,10 +23,12 @@ function App() {
     return (
         <div className={classes.root}>
             <AppBar>
-                <Typography variant="h6" className={classes.title}>
-                    Olivia Adams
-                </Typography>
-                <MenuButtons />
+                <Toolbar>
+                    <Typography variant="h6" className={classes.title}>
+                        Olivia Adams
+                    </Typography>
+                    <MenuButtons />
+                </Toolbar>
             </AppBar>
         </div>
     );
@@ -32,12 +36,10 @@ function App() {
 
 function MenuButtons() {
     return (
-        <>
+        <ButtonGroup aria-label="navigation" color="inherit" variant="text">
             <Button>Experience</Button>
             <Button>Posts</Button>
             <Button>Contact</Button>
-        </>
+        </ButtonGroup>
     );
 }
-
-export default App;
